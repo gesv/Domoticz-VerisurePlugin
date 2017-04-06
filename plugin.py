0269e3879ec2b3638607ae2ecef83692d301da4f
Geir Håkon Svendsen <gesv@tronder.net>
2017-04-06 19:38:53 +0200

    Initiell brun bubb

---
+++ plugin.py
@@ -0,0 +1,95 @@
+# Verisure Alarm plugin
+#
+# Author: Gesv and Daniko
+#
+"""
+<plugin key="Verisure" name="Verisure Alarm Central" author="gesv" version="1.0.0" wikilink="http://harmannenfalt.no" externallink="https://www.sau.no/">
+    <params>
+        <param field="Username" label="Username" width="200px" required="true" default=""/>
+        <param field="Password" label="Password" width="200px" required="true" default=""/>
+    </params>
+</plugin>
+"""
+
+from verisure import session
+import Domoticz
+
+class BasePlugin:
+    enabled = False
+    def __init__(self):
+        #self.var = 123
+        return
+
+    def onStart(self):
+        Domoticz.Log("onStart called")
+
+    def onStop(self):
+        Domoticz.Log("onStop called")
+
+    def onConnect(self, Status, Description):
+        Domoticz.Log("onConnect called")
+
+    def onMessage(self, Data, Status, Extra):
+        Domoticz.Log("onMessage called")
+
+    def onCommand(self, Unit, Command, Level, Hue):
+        Domoticz.Log("onCommand called for Unit " + str(Unit) + ": Parameter '" + str(Command) + "', Level: " + str(Level))
+
+    def onNotification(self, Name, Subject, Text, Status, Priority, Sound, ImageFile):
+        Domoticz.Log("Notification: " + Name + "," + Subject + "," + Text + "," + Status + "," + str(Priority) + "," + Sound + "," + ImageFile)
+
+    def onDisconnect(self):
+        Domoticz.Log("onDisconnect called")
+
+    def onHeartbeat(self):
+        Domoticz.Log("onHeartbeat called")
+
+global _plugin
+_plugin = BasePlugin()
+
+def onStart():
+    global _plugin
+    _plugin.onStart()
+
+def onStop():
+    global _plugin
+    _plugin.onStop()
+
+def onConnect(Status, Description):
+    global _plugin
+    _plugin.onConnect(Status, Description)
+
+def onMessage(Data, Status, Extra):
+    global _plugin
+    _plugin.onMessage(Data, Status, Extra)
+
+def onCommand(Unit, Command, Level, Hue):
+    global _plugin
+    _plugin.onCommand(Unit, Command, Level, Hue)
+
+def onNotification(Name, Subject, Text, Status, Priority, Sound, ImageFile):
+    global _plugin
+    _plugin.onNotification(Name, Subject, Text, Status, Priority, Sound, ImageFile)
+
+def onDisconnect():
+    global _plugin
+    _plugin.onDisconnect()
+
+def onHeartbeat():
+    global _plugin
+    _plugin.onHeartbeat()
+
+    # Generic helper functions
+def DumpConfigToLog():
+    for x in Parameters:
+        if Parameters[x] != "":
+            Domoticz.Debug( "'" + x + "':'" + str(Parameters[x]) + "'")
+    Domoticz.Debug("Device count: " + str(len(Devices)))
+    for x in Devices:
+        Domoticz.Debug("Device:           " + str(x) + " - " + str(Devices[x]))
+        Domoticz.Debug("Device ID:       '" + str(Devices[x].ID) + "'")
+        Domoticz.Debug("Device Name:     '" + Devices[x].Name + "'")
+        Domoticz.Debug("Device nValue:    " + str(Devices[x].nValue))
+        Domoticz.Debug("Device sValue:   '" + Devices[x].sValue + "'")
+        Domoticz.Debug("Device LastLevel: " + str(Devices[x].LastLevel))
+    return
---
+++ verisure/__init__.py
@@ -0,0 +1,12 @@
+"""
+A python module for reading and changing status of verisure devices through
+verisure app API.
+"""
+
+ALARM_ARMED_HOME = 'ARMED_HOME'
+ALARM_ARMED_AWAY = 'ARMED_AWAY'
+ALARM_DISARMED = 'DISARMED'
+LOCK_LOCKED = 'LOCKED'
+LOCK_UNLOCKED = 'UNLOCKED'
+SMARTPLUG_ON = 'on'
+SMARTPLUG_OFF = 'off'
